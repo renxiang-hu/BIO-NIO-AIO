2f6fb2c7cf3860ca377cf8e267859536fd143387
胡仁祥 <18917734365@163.com>
2023-02-15 17:27:46 +0800

    客户端发送消息，服务端接收消息

--- src/main/java/com/itheima/one/Client.java
+++ src/main/java/com/itheima/one/Client.java
@@ -4,16 +4,20 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
+/**
+ * 客户端
+ */
 public class Client {
     public static void main(String[] args) throws IOException {
-        //创建socket对象请求服务端的连接
-        Socket socket = new Socket("127.0.0.1", 9999);
-        //从socket对象中获得一个字节输出流
+        //1、创建Socket对象请求服务端的链接
+        Socket socket = new Socket("127.0.0.1",9999);
+        //2、从Socket对象中获取一个字节输出流
         OutputStream outputStream = socket.getOutputStream();
-        //把字节输出流包装成打印流
+        //3、把字节输出流包装成一个打印流
         PrintStream printStream = new PrintStream(outputStream);
-        printStream.print("hello world 服务器！");
+        printStream.println("hello world 服务器");
         printStream.flush();
     }
 }
--- src/main/java/com/itheima/one/Server.java
+++ src/main/java/com/itheima/one/Server.java
@@ -8,23 +8,22 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 /**
- * 目标：客户端发送消息，服务端接收信息
+ * 目标：客户端发送消息，服务端接收消息
  */
 public class Server {
     public static void main(String[] args) {
-        System.out.println("---服务端启动---");
         try {
-            //定义一个serverSocket对象进行服务器端的端口注册
+            //1、定义一个ServerSocket对象进行服务端的端口注册
             ServerSocket serverSocket = new ServerSocket(9999);
-            //监听客户端的socket连接请求
-            Socket accept = serverSocket.accept();
-            //从socket管道中得到一个字节输入流对象
-            InputStream inputStream = accept.getInputStream();
-            //把字节输入流包装成一个缓冲字符输入流
+            //2、监听客户端Socket链接请求
+            Socket socket = serverSocket.accept();
+            //3、从socket管道中得到一个字节输入流对象
+            InputStream inputStream = socket.getInputStream();
+            //4、把字节输入流包装成一个缓冲字符输入流
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
             String msg;
-            if ((msg= bufferedReader.readLine()) != null){
-                System.out.print("服务端接收到：" + msg);
+            while ((msg = bufferedReader.readLine()) != null) {
+                System.out.println("服务端接收到："+ msg);
             }
         } catch (IOException e) {
             e.printStackTrace();
